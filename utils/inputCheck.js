1ff29c50f2c156abfc94867d89a0768656ffc44f
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-03-24 20:18:23 -0400

    added change candidates party route

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,6 +1,7 @@
 // utility to check if an object has the required properties
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
+
 module.exports = function(obj, ...props) {
   const errors = [];
 
